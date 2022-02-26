ed8bb387d7012b3e987d38c219b6135750cd1cdb
Ayush Garg <ayush181000@gmail.com>
2022-02-27 03:19:48 +0530

    Create CountingSort.java

---
+++ Arrays/src/com/ayush/CountingSort.java
@@ -0,0 +1,34 @@
+package com.ayush;
+
+public class CountingSort {
+
+    public static void main(String[] args) {
+
+        int[] intArray = {2, 5, 8, 9, 2, 8, 7, 10, 4, 3};
+
+        countingSort(intArray, 1, 10);
+
+        countingSort(intArray, 0, intArray.length);
+
+        System.out.println(" ");
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.print(+intArray[i] + " ");
+        }
+    }
+
+    public static void countingSort(int[] input, int min, int max) {
+        int[] countingArray = new int[(max - min) + 1];
+
+        for (int i = 0; i < input.length; i++) {
+            countingArray[input[i] - min]++;
+        }
+
+        int j = 0;
+        for (int i = min; i <= max; i++) {
+            while (countingArray[i - min] > 0) {
+                input[j++] = i;
+                countingArray[i - min]--;
+            }
+        }
+    }
+}

0f96446e91809ad9a565c6fe7e2c7da42cb4a453
Ayush Garg <ayush181000@gmail.com>
2022-02-27 03:19:46 +0530

    Create Main.java

---
+++ Vectors/src/com/ayush/Main.java
@@ -0,0 +1,17 @@
+package com.ayush;
+
+import java.util.List;
+import java.util.Vector;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Employee> employeeList = new Vector<>();
+        employeeList.add(new Employee("Jane", "Jones", 123));
+        employeeList.add(new Employee("John", "Doe", 456));
+        employeeList.add(new Employee("Mary", "Smith", 789));
+        employeeList.add(new Employee("Mike", "Wilson", 256));
+
+    }
+}

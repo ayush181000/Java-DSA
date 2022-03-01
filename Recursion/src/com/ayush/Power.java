3674f3bfec86461cb57d7bd463a2ef68695acd51
Ayush Garg <ayush181000@gmail.com>
2022-03-01 14:12:15 +0530

    Create Power.java

---
+++ Recursion/src/com/ayush/Power.java
@@ -0,0 +1,22 @@
+package com.ayush;
+
+public class Power {
+
+    public static void main(String[] args) {
+
+        System.out.println(power(2, 2));
+    }
+
+    public static int power(int base, int exp) {
+
+        if (exp < 0) {
+            return -1;
+        }
+
+        if (exp == 0) {
+            return 1;
+        }
+
+        return base * power(base, exp - 1);
+    }
+}

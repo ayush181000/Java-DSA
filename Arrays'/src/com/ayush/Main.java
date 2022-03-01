cfdd4417f4a28397acdaf65fc668a0c688810e40
Ayush Garg <ayush181000@gmail.com>
2022-03-01 14:12:23 +0530

    Update Main.java

--- Arrays'/src/com/ayush/Main.java
+++ Arrays'/src/com/ayush/Main.java
@@ -3,21 +3,15 @@ package com.ayush;
 public class Main {
 
     public static void main(String[] args) {
-        int[] intArray = {1, 2, 3, 4, 5, 6, 7, 8};
-        rotateArray2(intArray, 2);
+        int[] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+        rotateArray3(intArray, 3);
 
         printArray(intArray);
 
     }
 
-    public static void printArray(int[] array) {
 
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
-        }
-        System.out.println("");
-    }
-
+    /* ROTATION OF ARRAY */
     public static void rotateArray1(int[] array, int d) {
 
         // Using temp array
@@ -44,7 +38,47 @@ public class Main {
     public static void rotateArray3(int[] array, int d) {
 
         // Juggling algorithm
-        
+        int n = array.length;
+        int j, k;
+
+        int gcd = findGCD(n, d);
+        System.out.println(gcd);
+
+
+        for (int i = 0; i < gcd; i++) {
+            int temp = array[i];
+            j = i;
+            while (true) {
+                k = j + d;
+                if (k >= n) {
+                    k = k - n;
+                }
+                if (k == i) {
+                    break;
+                }
+                array[j] = array[k];
+                j = k;
+            }
+            array[j] = temp;
+
+        }
 
     }
+
+    /* UTILITY FUNCTIONS */
+
+    public static void printArray(int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println("");
+    }
+
+    public static int findGCD(int a, int b) {
+        if (b == 0) return a;
+
+        return findGCD(b, a % b);
+    }
+
 }

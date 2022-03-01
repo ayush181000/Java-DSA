9e6641bd9c29800dc6351a99d3ded03fd3dce53c
Ayush Garg <ayush181000@gmail.com>
2022-03-01 14:12:20 +0530

    Create DecimalToBinary.java

---
+++ Recursion/src/com/ayush/DecimalToBinary.java
@@ -0,0 +1,21 @@
+package com.ayush;
+
+public class DecimalToBinary {
+
+    public static void main(String[] args) {
+        System.out.println(decimalToBinary(10));
+    }
+
+    public static int decimalToBinary(int n) {
+        if (n < 0) {
+            return -1;
+        }
+
+        if (n == 0) {
+            return 0;
+        }
+
+        return (n % 2) + (10 * (decimalToBinary(n / 2)));
+
+    }
+}

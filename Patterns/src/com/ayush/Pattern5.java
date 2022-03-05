5a9363a4205ea1a5fb3e7f2b3529edc37d6f104b
Ayush Garg <ayush181000@gmail.com>
2022-03-06 00:28:16 +0530

    Create Pattern5.java

---
+++ Patterns/src/com/ayush/Pattern5.java
@@ -0,0 +1,19 @@
+package com.ayush;
+
+public class Pattern5 {
+    public static void main(String[] args) {
+        int n = 4;
+        int i = 1;
+
+        while (i <= n) {
+            int j = 1;
+            while (j <= n) {
+                System.out.print((n - j + 1) + " ");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+
+}

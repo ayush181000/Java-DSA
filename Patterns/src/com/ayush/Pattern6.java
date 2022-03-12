59442cdeefcc77f8201415ec95be57b400f2fc6e
Ayush Garg <ayush181000@gmail.com>
2022-03-12 13:42:56 +0530

    Create Pattern6.java

---
+++ Patterns/src/com/ayush/Pattern6.java
@@ -0,0 +1,15 @@
+package com.ayush;
+
+public class Pattern6 {
+
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j > 0; j--) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}

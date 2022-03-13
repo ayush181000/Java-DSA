42ed5ac9e659e80021c93a69df32558e8f88349f
Ayush Garg <ayush181000@gmail.com>
2022-03-13 18:40:37 +0530

    .

---
+++ Strings/StringsDemo.java
@@ -0,0 +1,9 @@
+class StringsDemo {
+    public static void main(String[] args) {
+        char arr[] = { 'C', 'o', 'd', 'i', 'n', 'g' };
+        String str = "Coding";
+        String str1 = "".trim();
+        System.out.println(str1.length());
+        System.out.println(str.charAt(6));
+    }
+}

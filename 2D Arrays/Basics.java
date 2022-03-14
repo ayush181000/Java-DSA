36cf8aa8f60dd2d756ea178905ad8f061f508d4b
Ayush Garg <ayush181000@gmail.com>
2022-03-15 01:51:10 +0530

    .

---
+++ 2D Arrays/Basics.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Basics {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int row = s.nextInt();
+        int col = s.nextInt();
+
+        int[][] matrix = new int[row][col];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                matrix[i][j] = s.nextInt();
+            }
+        }
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}

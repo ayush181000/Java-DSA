24a0018d42ee004051ab04848ad1a4cef6ee43bf
Ayush Garg <ayush181000@gmail.com>
2022-03-15 00:56:05 +0530

    Update StringsDemo.java

--- Strings/StringsDemo.java
+++ Strings/StringsDemo.java
@@ -1,9 +1,21 @@
 class StringsDemo {
     public static void main(String[] args) {
         char arr[] = { 'C', 'o', 'd', 'i', 'n', 'g' };
-        String str = "Coding";
-        String str1 = "".trim();
-        System.out.println(str1.length());
-        System.out.println(str.charAt(6));
+        String str1 = "Coding";
+
+        String substr = str1.substring(1, 5);
+        System.out.println(substr);
+        System.out.println(str1.substring(6));
+
+        // String str2 = " is fun";
+        // String str3 = str1 + str2;
+        // str3 = "Coding";
+
+        // System.out.println(str1.concat(str2));
+        // System.out.println(str1.equals(str3));
+        // System.out.println(str1.compareTo(str2));
+        // System.out.println(str1 + str2);
+        // System.out.println(str1.length());
+        // System.out.println(str.charAt(6));
     }
 }

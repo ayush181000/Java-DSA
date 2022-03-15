f211a8c43d7f9a3f2a1dd2ca020f84767ad7bf8b
Ayush Garg <ayush181000@gmail.com>
2022-03-15 05:31:04 +0530

    Update Power.java

--- Recursion/src/com/ayush/Power.java
+++ Recursion/src/com/ayush/Power.java
@@ -1,10 +1,11 @@
-package com.ayush;
+// package com.ayush;
 
 public class Power {
 
     public static void main(String[] args) {
 
-        System.out.println(power(2, 2));
+        // System.out.println(power(2, 2));
+        System.out.println(powerBetter(13, 23));
     }
 
     public static int power(int base, int exp) {
@@ -18,5 +19,21 @@ public class Power {
         }
 
         return base * power(base, exp - 1);
+    }
+
+    public static int powerBetter(int x, int n) {
+        if (x == 0 && n == 0)
+            return 1;
+        if (x == 0)
+            return 0;
+        if (n == 0)
+            return 1;
+        int smallAns = powerBetter(x, n / 2);
+        if (n % 2 == 0) {
+            return smallAns * smallAns;
+        } else {
+            return x * smallAns * smallAns;
+        }
+
     }
 }

ae5f20a60fb48186be7c619fc40425b4ac1f06db
Ayush Garg <ayush181000@gmail.com>
2022-05-17 10:48:57 +0530

    Update BinaryTreeUse.java

--- Binary Tree/BinaryTreeUse.java
+++ Binary Tree/BinaryTreeUse.java
@@ -376,6 +376,21 @@ public class BinaryTreeUse {
 
     }
 
+    public static void elementsInRangeK1K2(BinaryTreeNode<Integer> root, int k1, int k2) {
+        if (root == null) {
+            return;
+        }
+        if (root.data >= k1 && root.data <= k2) {
+            elementsInRangeK1K2(root.left, k1, k2);
+            System.out.println(root.data + " ");
+            elementsInRangeK1K2(root.right, k1, k2);
+        } else if (root.data < k1) {
+            elementsInRangeK1K2(root.right, k1, k2);
+        } else if (root.data > k1) {
+            elementsInRangeK1K2(root.left, k1, k2);
+        }
+    }
+
     public static void main(String[] args) {
         /**
          * recursion input

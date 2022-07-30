47e3b00fa0cf7a7569f0c2e1ff29b5f4597763cc
Ayush Garg <ayush181000@gmail.com>
2022-07-30 16:45:57 +0530

    Create MinInsertionToMakePalindrome.java

---
+++ Dynamic Programming/MinInsertionToMakePalindrome.java
@@ -0,0 +1,27 @@
+// https://www.codingninjas.com/codestudio/problems/minimum-insertions-to-make-palindrome_985293?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos
+
+public class MinInsertionToMakePalindrome {
+
+}
+
+class Solution {
+    public static int minInsertion(String str) {
+        int n = str.length();
+        String t = new StringBuilder(str).reverse().toString();
+
+        int dp[][] = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (str.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return n - dp[n][n];
+    }
+
+}

be6c8101cc0b1910657cc4fa7c064303dfa292ad
Ayush Garg <ayush181000@gmail.com>
2022-07-30 16:45:59 +0530

    Create MinNoOfDelAndInsertions.java

---
+++ Dynamic Programming/MinNoOfDelAndInsertions.java
@@ -0,0 +1,30 @@
+// https://www.codingninjas.com/codestudio/problems/can-you-make_4244510?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos&leftPanelTab=1
+
+public class MinNoOfDelAndInsertions {
+
+}
+
+class Solution {
+    public static int canYouMake(String str, String ptr) {
+        return str.length() + ptr.length() - (2 * lcs(str, ptr));
+    }
+
+    public static int lcs(String str, String t) {
+        int n = str.length();
+        int m = t.length();
+        int dp[][] = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (str.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+
+}
